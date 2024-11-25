22dcd0c43a3ecf3f34f30d8a5764181f686224e7
Nikolay Ivanchenko <ishaltbecome@gmail.com>
2024-11-25 21:06:56 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const os = require("os");
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+const favicon = require("serve-favicon");
+
+const routes = require("./routes");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(favicon(path.join(__dirname, "public", "favicon.ico")))
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(routes);
+
+const PORT = 3000;
+
+app.listen(PORT, () => console.log(`Server running at localhost:${PORT}`));
